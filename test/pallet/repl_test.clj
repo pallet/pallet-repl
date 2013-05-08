8ee5603c728e58cb6b3f9a56be7d0d74f99ae0bb
Hugo Duncan <hugo@hugoduncan.org>
2013-05-08 15:10:02 -0400

    Add simple test for explain-plan

--- test/pallet/repl_test.clj
+++ test/pallet/repl_test.clj
@@ -1,6 +1,12 @@
 (ns pallet.repl-test
-  (:use clojure.test)
-  (:require [pallet.repl :as repl]))
+  (:require
+   [clojure.test :refer :all]
+   [pallet.actions :refer [exec-script]]
+   [pallet.api :refer [plan-fn]]
+   [pallet.repl :as repl]))
 
 (deftest use-test
   (repl/use-pallet))
+
+(deftest explain-plan-test
+  (is (with-out-str (repl/explain-plan (plan-fn (exec-script ("ls")))))))
